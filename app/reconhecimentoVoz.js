b11d63627f07090208f149ed2557f28b9fca5544
Matheus Estevão <matheus935@gmail.com>
2023-07-28 01:49:38 -0300

    Exibindo o numero falado na tela

--- app/reconhecimentoVoz.js
+++ app/reconhecimentoVoz.js
@@ -1,3 +1,5 @@
+const elementoChute = document.getElementById('chute');
+
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
@@ -7,5 +9,14 @@ recognition.start();
 recognition.addEventListener('result', onSpeak)
 
 function onSpeak (e) {
-    console.log(e.results[0][0].transcript)
+   chute = e.results[0][0].transcript;
+   exibeChute(chute);
+}
+
+function exibeChute (chute) {
+    elementoChute.innerHTML = `
+    <div>Você disse: </div>
+    <span class="box">${chute}</span>
+    <div>O numero secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+    `
 }
