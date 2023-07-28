9ea3143db92c53e9ab2e59ddf35ab6a0e3f9afe3
Matheus Estevão <matheus935@gmail.com>
2023-07-28 03:19:41 -0300

    botão de jogar novamente e tela de game over

--- app/validações.js
+++ app/validações.js
@@ -2,9 +2,21 @@ function validaChute (chute) {
     const numero = +chute;
 
     if(chuteInvalido(numero)){
+        if(chute === 'game over')
+        {
+            document.body.innerHTML = `
+            <h1> Game Over <h1>
+            <h3> Você Desistiu <h3>
+   
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+           
+           ` 
+
+        } else {
         elementoChute.innerHTML += `
         <div> Valor Invalido </div>
         `
+        }
     }
     if(chuteForaValor(numero)) {
        elementoChute.innerHTML += `
@@ -24,7 +36,7 @@ function validaChute (chute) {
         elementoChute.innerHTML += `
         <div>O numero secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
         `
-     } else {
+     } else if (numero < numeroSecreto) {
         elementoChute.innerHTML += `
         <div>O numero secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
         `
